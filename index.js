e75cbe3d0909b97098b87082ef49c79c5859d4f9
josephquested <josephquested@gmail.com>
2016-10-06 21:27:16 +1300

    add random frasier functionality

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById('random-button').addEventListener("click", openRandomEpisode)
+
+function openRandomEpisode () {
+  var season = randomInt(1, 11)
+  var episode = randomInt(1, 24)
+  open(`http://putlocker.is/watch-frasier-tvshow-season-${season}-episode-${episode}-online-free-putlocker.html`)
+}
+
+function randomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
